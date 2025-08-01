df3f336bece17dc0bae111fb6de004785c6980cd
DimDeveloper18 <dimash.itcout@gmail.com>
2025-08-01 11:51:51 +0100

    Variable 'calorie_table' added with dictionary values, then veg_page function created if,else statement code for calories calculation.

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+calorie_table = {"Avocado": 1.6, "Artichoke": 0.5}
+
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("/vegetables")
+@app.route("/vegetables", methods=["GET", "POST"])
 def veg_page():
-    return render_template("vegetables.html")
+    calories_total = ""
+    if request.method == "POST":
+        vegetable = request.form.get("vegetable")
+        veg_gramm = request.form.get("gramm")
+        gramm = float(veg_gramm)
+        calories_total = round(gramm * calorie_table.get(vegetable, 0), 2)
+    else:
+            calories_total = "Invalid input"
+    return render_template("vegetables.html", calories=calories_total)
 
 @app.route("/meat")
 def meat_page():
